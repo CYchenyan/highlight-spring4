87939d8435b8af39e9e69dc0ce32b7554cae5907
Chen Yan <yan.chen@thoughtworks.com>
2019-12-26 16:39:28 +0800

    java configuration

--- src/main/java/com/wisely/highlight_spring4/ch1/di/Main.java
+++ src/main/java/com/wisely/highlight_spring4/ch1/di/Main.java
@@ -3,7 +3,8 @@ package com.wisely.highlight_spring4.ch1.di;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
-    public static void Main(String args[]) {
+
+    public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DiConfig.class);
 
         UseFunctionService useFunctionService = context.getBean(UseFunctionService.class);
