ff7dec37ff37a53478bf892441dd2515335d9710
Chen Yan <yan.chen@thoughtworks.com>
2019-12-26 15:52:09 +0800

    init @server bean

---
+++ src/main/java/com/wisely/highlight_spring4/HighlightSpring4Application.java
@@ -0,0 +1,12 @@
+package com.wisely.highlight_spring4;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighlightSpring4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighlightSpring4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/wisely/highlight_spring4/HighlightSpring4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wisely.highlight_spring4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighlightSpring4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
